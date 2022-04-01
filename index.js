c196bfe4efee6ed013bfa1c55d8fc46cb60a99b3
Mike <56912424+webml@users.noreply.github.com>
2022-04-01 08:25:42 +0300

    Update JS, CSS and HTML
    Create new block, update js syntax

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-function whatIsSeasons(month = Number(prompt("Введите номер месяца"))) {
+function guessTheSeason(month = Number(prompt("Введите номер месяца"))) {
   if (month === 1 || month === 2 || month === 12) {
-    alert("Зима");
-  } else if (month === 3 || month === 4 || month === 5) {
-    alert("Весна");
-  } else if (month === 6 || month === 7 || month === 8) {
-    alert("Лето");
-  } else if (month === 9 || month === 10 || month === 11) {
-    alert("Осень");
-  } else {
-    alert("Введите число от 1 до 12");
+    if (confirm("Это зима. Хотите сыграть ещё раз?")) {
+      guessTheSeason();
+    }
+    return;
   }
+
+  if (month === 3 || month === 4 || month === 5) {
+    if (confirm("Это весна. Хотите сыграть ещё раз?")) {
+      guessTheSeason();
+    }
+    return;
+  }
+
+  if (month === 6 || month === 7 || month === 8) {
+    if (confirm("Это лето. Хотите сыграть ещё раз?")) {
+      guessTheSeason();
+    }
+    return;
+  }
+
+  if (month === 9 || month === 10 || month === 11) {
+    if (confirm("Это осень. Хотите сыграть ещё раз?")) {
+      guessTheSeason();
+    }
+    return;
+  }
+
+  alert("Введите число от 1 до 12");
 }
